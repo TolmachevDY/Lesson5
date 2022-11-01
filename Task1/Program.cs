f712a95068b6e180da8101d20f4ee83d1bb1317b
Tolmachev DY <Tolmachev DY@DESKTOP-5RJHKAS>
2022-11-01 22:45:32 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа массива");
+            int[] arrey = new int[7];
+            int s=0;
+            int k=0;
+            for (int i = 0; i < 7; i++)
+            {
+                arrey[i]=Convert.ToInt32(Console.ReadLine());
+                s += arrey[i];
+                k++;
+            }
+            Console.WriteLine("Среднее арифметическое равно {0:f3}",((double)s)/k);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] arrey = new int[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arrey[i, j] = (((i + j) % 2 == 0)) ? 1:0;
+                    Console.Write("{0}", arrey[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
